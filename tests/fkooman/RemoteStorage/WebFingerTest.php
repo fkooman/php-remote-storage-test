03416e5f98a425c2bdaa54874b6ddd667e51a601
François Kooman <fkooman@tuxed.net>
2014-10-02 10:56:42 +0200

    use release of fkooman/webfinger, fix api usage by tests

--- tests/fkooman/RemoteStorage/WebFingerTest.php
+++ tests/fkooman/RemoteStorage/WebFingerTest.php
@@ -17,8 +17,43 @@ class WebFingerTest extends PHPUnit_Framework_TestCase
 
         $this->assertInternalType('string', $webFingerData->getHref('remotestorage'));
         // we want an auth URI
-        $this->assertArrayHasKey('http://tools.ietf.org/html/rfc6749#section-4.2', $webFingerData->getProperties('remotestorage'));
+        $this->assertNotNull($webFingerData->getProperty('remotestorage', 'http://tools.ietf.org/html/rfc6749#section-4.2'));
         // we need a version
-        $this->assertArrayHasKey('http://remotestorage.io/spec/version', $webFingerData->getProperties('remotestorage'));
+        $this->assertNotNull($webFingerData->getProperty('remotestorage', 'http://remotestorage.io/spec/version'));
     }
 }
+
+#    private function validateLinkRelation($linkRelation)
+#    {
+#        // additional checks for remotestorage link relation
+#        if ("remotestorage" === $linkRelation) {
+#            // needs href
+#            if (null === $this->getHref($linkRelation)) {
+#                throw new WebFingerException("remotestorage needs 'href'");
+#            }
+#            // needs properties
+#            $properties = $this->getProperties($linkRelation);
+#            if (null === $properties) {
+#                throw new WebFingerException("remotestorage needs 'properties'");
+#            }
+
+#            // needs authUri property
+#            $this->requireStringKeyValue($properties, 'http://tools.ietf.org/html/rfc6749#section-4.2', true);
+#            $this->requireUri($properties['http://tools.ietf.org/html/rfc6749#section-4.2']);
+
+#            // needs version property
+#            $this->requireStringKeyValue($properties, 'http://remotestorage.io/spec/version', true);
+
+#            // optional properties
+#            if (null !== $this->requireStringKeyValue($properties, 'https://tools.ietf.org/html/rfc2616#section-14.16')) {
+#                if (!in_array($properties['https://tools.ietf.org/html/rfc2616#section-14.16'], array("true", "false"))) {
+#                    throw new WebFingerException("'property needs to be 'true' or 'false' as string");
+#                }
+#            }
+#            if (null !== $this->requireStringKeyValue($properties, 'http://tools.ietf.org/html/rfc6750#section-2.3')) {
+#                if (!in_array($properties['http://tools.ietf.org/html/rfc6750#section-2.3'], array("true", "false"))) {
+#                    throw new WebFingerException("'property needs to be 'true' or 'false' as string");
+#                }
+#            }
+#        }
+#    }
